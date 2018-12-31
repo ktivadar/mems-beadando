76476a7f06b5dc2a3b15e9d388afa525eb0bf2ec
tivadark <43910197+tivadark@users.noreply.github.com>
2018-12-31 02:17:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $conn->close();
 <!DOCTYPE html>  
 <html>  
 <head>  
-<title>KAMONDY</title>  
+<title>Projekt</title>  
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">  
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js"></script>  
 <script src="https://code.highcharts.com/highcharts.js"></script>  
