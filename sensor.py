a84a9c01af6b562122012b083b741df227160112
tivadark <43910197+tivadark@users.noreply.github.com>
2019-01-08 00:30:17 +0100

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -6,32 +6,30 @@ import datetime
 import glob  
 import MySQLdb  
 from time import strftime  
-#from Adafruit_BME280 import *  
 from sense_hat import SenseHat
 
 sense = SenseHat()
 sense.clear()
   
-#sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)  
   
-#Variables for MySQL  
+#Valtozok megadasa MySQLnek 
 db = MySQLdb.connect(host="localhost", user="root", passwd="mysql-jelszo", db="sensor") # replace password with your password  
 cur = db.cursor()  
   
-def dateTime(): #get UNIX time  
+def dateTime(): # UNIX ido beallitasa  
         secs = float(time.time())  
         secs = secs*1000  
         return secs  
   
-def tempRead(): #read temperature, return float with 3 decimal places  
+def tempRead(): #homerseklet kiolvasasa, float 3 decimalis  
         degrees = float('{0:.3f}'.format(sense.get_temperature()-10.5))
         return degrees  
   
-def pressRead():#read pressure, return float with 3 decimal places  
+def pressRead():#legnyomas kiolvasasa, float 3 decimalis   
         pascals = float('{0:.3f}'.format(sense.get_pressure()))  
         return pascals  
   
-def humidityRead(): #read humidity, return float with 3 decimal places  
+def humidityRead(): #paratartalom kiolvasasa, float 3 decimalis   
         humidity = float('{0:.3f}'.format(sense.get_humidity()))  
         return humidity  
   
@@ -43,14 +41,14 @@ humidity = humidityRead()
 sql = ("""INSERT INTO bmesensor (datetime,temperature,pressure,humidity) VALUES (%s,%s,%s,%s)""", (secs, temperature, pressure, humidity))  
   
 try:  
-    print "Writing to the database..."  
+    print "Adatbazisba iras..."  
     cur.execute(*sql)  
     db.commit()  
-    print "Write complete"  
+    print "Iras kesz"  
   
 except:  
     db.rollback()  
-    print "We have a problem"  
+    print "Nem sikerult"  
   
 cur.close()  
 db.close()  
